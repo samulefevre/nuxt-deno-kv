cb88c00baa53942d57f71ed93e38f5dbbbce3dd7
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-12-20 02:55:35 +0100

    Refactor useEventSource function to use newDatas variable

--- composables/useEventSource.ts
+++ composables/useEventSource.ts
@@ -4,20 +4,20 @@ type EventData = {
 }
 
 export const useEventSource = (eventData: EventData, datas: any[]) => {
-    const data = ref(datas)
+    const newDatas = ref(datas)
 
     switch (eventData.status) {
         case 'added':
             let arr = [eventData.value, ...datas]
             let uniqueArr = Array.from(new Set(arr.map(obj => obj.id))).map(id => arr.find(obj => obj.id === id));
 
-            data.value = uniqueArr
+            newDatas.value = uniqueArr
             break;
         case 'deleted':
-            data.value = datas.filter((item: any) => item.id !== eventData.value.id)
+            newDatas.value = datas.filter((item: any) => item.id !== eventData.value.id)
             break;
         case 'updated':
-            data.value = datas.map((item: any) => {
+            newDatas.value = datas.map((item: any) => {
                 if (item.id === eventData.value.id) {
                     return eventData.value
                 }
@@ -28,5 +28,5 @@ export const useEventSource = (eventData: EventData, datas: any[]) => {
             break;
     }
 
-    return data
+    return newDatas
 }
