ce8147ecd7630c1b4c7e0415c655e5301aba15dc
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-12-20 02:16:25 +0100

    update UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,36 @@
 <template>
-  <div>
-    <!-- <UButton @click="deleteTodo">Delete</UButton> -->
-    <h1>My App</h1>
-    <UInput v-model="state.todo" name="todo" />
-    <pre>{{ state }}</pre>
-    <UButton @click="addTodo">Add Todo</UButton>
-    <div>
-      <h2>Todos</h2>
-      <div v-for="todo of todos" :key="todo.id">
-        <div class="flex flex-row gap-4 mt-4">
-          <div>
-            {{ todo.text }} - {{ todo.createdAt }}
-          </div>
-          <div>
-            <UToggle :model-value="Boolean(todo.completed)" @update:model-value="toggleTodo(todo)" />
-          </div>
-          <div>
-            <UButton @click="deleteTodo(todo.id)">Delete</UButton>
-          </div>
+  <UContainer class="py-8">
+    <h1 class="font-bold text-3xl">Todos</h1>
+    <form class="mt-6 flex gap-2" @submit.prevent="addTodo">
+      <UInput v-model="state.todo" size="xl" required placeholder="Add a todo item" class="flex-1" />
+      <UButton size="xl" type="submit" color="green" icon="i-heroicons-plus-20-solid" :loading="loading" />
+    </form>
+    <ul class="pt-6 divide-y divide-gray-200 dark:divide-gray-800">
+      <li v-for="todo of todos" :key="todo.id" class="flex items-center gap-4 py-2">
+        <div class="flex-1">
+          <h3 class="text-lg font-medium" :class="[todo.completed ? 'line-through text-gray-500' : '']">
+            {{ todo.text }}
+          </h3>
+          <span class="text-sm">
+            <UseTimeAgo v-slot="{ timeAgo }" :time="todo.createdAt">Created {{ timeAgo }}</UseTimeAgo>
+            <UseTimeAgo v-if="todo.createdAt !== todo.updatedAt" v-slot="{ timeAgo }" :time="todo.updatedAt">· Updated {{
+              timeAgo }}</UseTimeAgo>
+          </span>
         </div>
-      </div>
-    </div>
-  </div>
+
+        <UToggle :model-value="Boolean(todo.completed)" @update:model-value="toggleTodo(todo)" />
+
+        <UButton @click="deleteTodo(todo.id)" icon="i-heroicons-x-mark-20-solid" color="red" variant="soft" size="xl" />
+      </li>
+    </ul>
+  </UContainer>
 </template>
   
 <script setup lang="ts">
 import type { Todos } from '~/server/types/db';
+import { UseTimeAgo } from '@vueuse/components'
+
+const { copy } = useCopyToClipboard()
 
 const state = reactive({
   todo: undefined,
@@ -33,13 +38,20 @@ const state = reactive({
 
 const { data: todos } = await useFetch('/api/todos')
 
+const loading = ref(false)
+
 const addTodo = async () => {
   if (!state.todo) return
+
+  loading.value = true
 
   await $fetch('/api/todos', {
     method: 'POST',
     body: { text: state.todo },
   })
+
+  loading.value = false
+  state.todo = undefined
 }
 
 const deleteTodo = async (id: string) => {
